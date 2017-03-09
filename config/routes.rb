312a4deab1a0751c08d8547d06992f2b53345e7a
Ricardo <rlira2@uc.cl>
2017-03-09 01:39:06 -0300

    all ok with heroku and admin ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :announcements
-  get 'index' => 'announcements#index'
   root 'announcements#main_page'
+
+  get 'index' => 'announcements#index'
+  get 'main_page' => 'announcements#main_page'
+
 end
